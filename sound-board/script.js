90b341966c964171d0442ad600dbd04b5683b3ef
lukes <lads89@gmail.com>
2020-12-05 22:16:35 +0000

    feat(sound-board) added soundboard project.

---
+++ sound-board/script.js
@@ -0,0 +1,20 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+sounds.forEach((sound) => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+    btn.innerText = sound;
+    btn.addEventListener('click', () => {
+        stopAudio();
+        document.getElementById(sound).play();
+    });
+    document.getElementById('buttons').appendChild(btn);
+});
+
+function stopAudio() {
+    sounds.forEach((sound) => {
+        const audio = document.getElementById(sound);
+        audio.pause();
+        audio.currentTime = 0;
+    });
+}
