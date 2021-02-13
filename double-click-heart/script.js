537b02e9caf1c411611709ed4a78c5faffbc2e10
lukes <lads89@gmail.com>
2021-02-13 22:32:32 +0000

    feat(double-heart): add project.

---
+++ double-click-heart/script.js
@@ -0,0 +1,42 @@
+const loveMe = document.querySelector('.loveMe');
+const times = document.querySelector('#times');
+
+let clickTime = 0;
+let timesClicked = 0;
+
+loveMe.addEventListener('click', (e) => {
+    if (clickTime === 0) {
+        clickTime = new Date().getTime();
+    } else {
+        if (new Date().getTime() - clickTime < 800) {
+            createHeart(e);
+            clickTime = 0;
+            timesClicked++;
+        } else {
+            clickTime = new Date().getTime();
+        }
+    }
+});
+
+const createHeart = (event) => {
+    const heart = document.createElement('i');
+    heart.classList.add('fas');
+    heart.classList.add('fa-heart');
+
+    const x = event.clientX;
+    const y = event.clientY;
+
+    const leftOffset = event.target.offsetLeft;
+    const topOffset = event.target.offsetTop;
+
+    const xInside = x - leftOffset;
+    const yInside = y - topOffset;
+
+    heart.style.top = `${yInside}px`;
+    heart.style.left = `${xInside}px`;
+
+    loveMe.appendChild(heart);
+    times.innerHTML = timesClicked;
+
+    setTimeout(() => heart.remove(), 1000);
+};
